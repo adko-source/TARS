646e990d7061932046b9f2637d437439973aa4f1
Adam Marko <adko87@hotmail.com>
2024-05-11 11:02:21 -0600

    add method for saving excel data to vars

--- app.py
+++ app.py
@@ -2,25 +2,37 @@ import pandas as pd
 
 print("app started!")
 
-
-
 data = {'Name': ['Alice', 'Bob', 'Charlie'],
         'Age': [25, 30, 35]}
 
-df = pd.DataFrame(data)
-print(df)
+# df = pd.DataFrame(data)
+# print(df)
 
 excel_file_path = "Unprocessed Files/lines.xlsx"
 
 # Read the Excel file into a DataFrame
-lines = pd.read_excel(excel_file_path)
+df = pd.read_excel(excel_file_path)
 
 # Display the DataFrame
-print(lines)
+# print(df)
 
-for index, row in lines.iterrows():
-    for column in lines.columns:
+for index, row in df.iterrows():
+    for column in df.columns:
         cell_value = row[column]
-        # Do something with the cell value (e.g., save it to a variable)
-        variable_name = f"{column}_{index}"  # Example variable name
-        locals()[variable_name] = cell_value  # Save value to a variable
+        x = f"{column}: {cell_value}"  
+        locals()[x] = cell_value  
+        print(x)
+
+  # Adjust the variable name as needed
+
+# df_id = df["Line ID"]
+# print(df_id)
+
+# line_id_first_row = df.loc[0, "Line ID"]
+# print(line_id_first_row)
+
+# text_second_row = df.loc[1, "Text"]
+# print(text_second_row)
+
+# for index in df.iterrows():
+#     print(df.loc[index, "Line ID"])
