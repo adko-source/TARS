ae553ff6fc0a722bbfb3b5ad37169d4b230856f5
Adam Marko <adko87@hotmail.com>
2024-05-11 19:50:57 -0600

    add requirements.txt and test api call

--- app.py
+++ app.py
@@ -1,4 +1,14 @@
 import pandas as pd
+import requests
+import json
+
+with open('config.json') as config_file:
+    config = json.load(config_file)
+
+api_key = config['api_key']
+
+print(api_key)
+
 
 print("app started!")
 
@@ -20,8 +30,39 @@ for index, row in df.iterrows():
     for column in df.columns:
         cell_value = row[column]
         x = f"{column}: {cell_value}"  
-        locals()[x] = cell_value  
+        # locals()[x] = cell_value  
         print(x)
+
+
+
+url = "https://api.elevenlabs.io/v1/text-to-speech/{voice_id}"
+
+payload = {
+    "text": "<string>",
+    "model_id": "<string>",
+    "voice_settings": {
+        "stability": 123,
+        "similarity_boost": 123,
+        "style": 123,
+        "use_speaker_boost": True
+    },
+    "pronunciation_dictionary_locators": [
+        {
+            "pronunciation_dictionary_id": "<string>",
+            "version_id": "<string>"
+        }
+    ],
+    "seed": 123,
+    "previous_text": "<string>",
+    "next_text": "<string>",
+    "previous_request_ids": ["<string>"],
+    "next_request_ids": ["<string>"]
+}
+headers = {"Content-Type": "application/json"}
+
+response = requests.request("POST", url, json=payload, headers=headers)
+
+print(response.text)
 
   # Adjust the variable name as needed
 
