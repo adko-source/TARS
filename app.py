b5e28948949d972e3d5af11d9e76f79ca4933b6c
Adam Marko <adko87@hotmail.com>
2024-05-12 12:18:52 -0600

    remove unused code

--- app.py
+++ app.py
@@ -68,19 +68,6 @@ response_text = call_text_to_speech_api(
     voice_id="5Q0t7uMcjvnagumLfvZi",
     text="Hello Ryan"
     )    
-# def save_voices_to_file(data):
-
-#     for voice in data['voices']:
-#   # For each 'voice', the 'name' and 'voice_id' are printed out. 
-#   # These keys in the voice dictionary contain values that provide information about the specific voice.
-#         # print(f"{voice['name']}; {voice['voice_id']}")
-
-#         with open('voices.txt', 'a') as file:
-#     # Iterate over each voice dictionary in the list
-           
-#           file.write(f"{voice['name']}; {voice['voice_id']}\n")
-    
-#     return
 
 # Get the list of available voices from API
 def get_voices():
@@ -104,40 +91,7 @@ def get_voices():
 
     # Save the available voices returned from the api to an Excel file
     df.to_excel('voices.xlsx', index=False)
-
-    # save_voices_to_file(response_json)
     
     return voices
 
 get_voices()
-
-
-
-
-#call_text_to_speech_api(voice_id="2")
-
-# response_text = call_text_to_speech_api(
-#     voice_id="AZnzlk1XvdvUeBnXmlld",
-#     text="Hello World",
-#     model_id="eleven_monolingual_v1"
-   
-# )
-
-# print(response_text)
-# response = requests.request("POST", url, json=payload, headers=headers)
-
-# print(response.text)
-
-  # Adjust the variable name as needed
-
-# df_id = df["line_id"]
-# print(df_id)
-
-# line_id_first_row = df.loc[0, "Line ID"]
-# print(line_id_first_row)
-
-# text_second_row = df.loc[1, "Text"]
-# print(text_second_row)
-
-# for index in df.iterrows():
-#     print(df.loc[index, "Line ID"])
