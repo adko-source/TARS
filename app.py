0e1dcd4322c331ab7fff928c8a966bf3be5f7e56
Adam Marko <adko87@hotmail.com>
2024-05-12 10:05:30 -0600

    add function to call text to speech api

--- app.py
+++ app.py
@@ -2,29 +2,19 @@ import pandas as pd
 import requests
 import json
 
+# Get API key from config.json
 with open('config.json') as config_file:
     config = json.load(config_file)
 
 api_key = config['api_key']
 
-print(api_key)
-
-
 print("app started!")
 
-data = {'Name': ['Alice', 'Bob', 'Charlie'],
-        'Age': [25, 30, 35]}
-
-# df = pd.DataFrame(data)
-# print(df)
 
 excel_file_path = "unprocessed_files/lines.xlsx"
 
 # Read the Excel file into a DataFrame
 df = pd.read_excel(excel_file_path)
-
-# Display the DataFrame
-# print(df)
 
 for index, row in df.iterrows():
     for column in df.columns:
@@ -33,7 +23,7 @@ for index, row in df.iterrows():
         # locals()[x] = cell_value  
         print(x)
 
-def display_voices(data):
+def save_voices_to_file(data):
 
     for voice in data['voices']:
   # For each 'voice', the 'name' and 'voice_id' are printed out. 
@@ -64,53 +54,69 @@ def get_voices():
 
     voices = response_json["voices"]
 
+    # Convert voices json into a DataFrame
     df = pd.DataFrame(voices)
 
     # Write the DataFrame to an Excel file
     df.to_excel('voices.xlsx', index=False)
 
-    display_voices(response_json)
+    save_voices_to_file(response_json)
     
     return voices
-
-# print(get_voices())
 
 get_voices()
 
 
+def call_text_to_speech_api(voice_id, text, model_id):
 
-# print(get_voices())
+    print(voice_id)
+    url = f"https://api.elevenlabs.io/v1/text-to-speech/{voice_id}/stream"
 
-payload = {
-    "text": "<string>",
-    "model_id": "<string>",
-    "voice_settings": {
-        "stability": 123,
-        "similarity_boost": 123,
-        "style": 123,
-        "use_speaker_boost": True
-    },
-    "pronunciation_dictionary_locators": [
-        {
-            "pronunciation_dictionary_id": "<string>",
-            "version_id": "<string>"
-        }
-    ],
-    "seed": 123,
-    "previous_text": "<string>",
-    "next_text": "<string>",
-    "previous_request_ids": ["<string>"],
-    "next_request_ids": ["<string>"]
-}
-headers = {"Content-Type": "application/json"}
+    payload = {
+        "text": text,
+        "model_id": "<string>",
+        "voice_settings": {
+            "stability": 123,
+            "similarity_boost": 123,
+            "style": 123,
+            "use_speaker_boost": True
+        },
+        "pronunciation_dictionary_locators": [
+            {
+                "pronunciation_dictionary_id": "<string>",
+                "version_id": "<string>"
+            }
+        ],
+        "seed": 123,
+        "previous_text": "<string>",
+        "next_text": "<string>",
+        "previous_request_ids": ["<string>"],
+        "next_request_ids": ["<string>"]
+    }
+    headers = {"Content-Type": "application/json"}
 
+    response = requests.request("POST", url, json=payload, headers=headers)
+    
+    # print(response.text)
+    return response.text
+
+#call_text_to_speech_api(voice_id="2")
+
+response_text = call_text_to_speech_api(
+    voice_id="AZnzlk1XvdvUeBnXmlld",
+    text="Hello World",
+    model_id="eleven_monolingual_v1"
+   
+)
+
+print(response_text)
 # response = requests.request("POST", url, json=payload, headers=headers)
 
 # print(response.text)
 
   # Adjust the variable name as needed
 
-# df_id = df["Line ID"]
+# df_id = df["line_id"]
 # print(df_id)
 
 # line_id_first_row = df.loc[0, "Line ID"]
